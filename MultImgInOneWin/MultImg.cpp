b88d2de54022ef18a2887c8e3f2864aa666098d2
Hanli0 <x5489134@vip.qq.com>
2016-11-14 14:36:55 +0800

    #Version 2.1

--- MultImgInOneWin/MultImg.cpp
+++ MultImgInOneWin/MultImg.cpp
@@ -11,8 +11,8 @@ int main()
 	Mat left, right;
 	Mat canvas;
 	int w, h;
-	w = 640;
-	h = 480;
+	w = 320;
+	h = 240;
 
 	VideoCapture capleft(1);
 	VideoCapture capright(0);
@@ -22,18 +22,25 @@ int main()
 		capleft >> left;
 		capright >> right;
 
-		canvas.create(h, w * 2, CV_8UC3);
+		canvas.create(h*2, w * 2, CV_8UC3);
 
-		//左图像画到画布上
-
-		//得到画布的一部分 
-		Mat canvasPart = canvas(Rect(w * 0, 0, w, h));
+		//左上图像画到画布上
+        //得到画布的一部分 
+		Mat canvasPart = canvas(Rect(0, 0, w, h));
 		//把图像缩放到跟canvasPart一样大小
 		resize(left, canvasPart, canvasPart.size(), 0, 0, INTER_AREA);
-		//右图像画到画布上
-
-		//获得画布的另一部分
+		
+		//右上图像画到画布上
+        //获得画布的另一部分
 		canvasPart = canvas(Rect(w, 0, w, h));
+		resize(right, canvasPart, canvasPart.size(), 0, 0, INTER_LINEAR);
+
+		//左下图像画到画布上
+		canvasPart = canvas(Rect(0, h, w, h));
+		resize(left, canvasPart, canvasPart.size(), 0, 0, INTER_LINEAR);
+
+		//右下图像画到画布上
+		canvasPart = canvas(Rect(w, h, w, h));
 		resize(right, canvasPart, canvasPart.size(), 0, 0, INTER_LINEAR);
 
 		imshow("Output", canvas);
